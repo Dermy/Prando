49b3384b307e75ad7e8a841043c5cb9514c8391c
Dermy <Dermy@users.noreply.github.com>
2014-07-05 05:01:59 -0400

    version bump, switch to semantic versioning

--- src/prando/Prando.java
+++ src/prando/Prando.java
@@ -35,7 +35,7 @@ import prando.util.Utilities;
 
 public class Prando
 {
-    public static final double VERSION = 2.1;
+    public static final String VERSION = "2.1.1";
 
     private String arg;
 
